58a9ec09c10be02ec7091f80de4cdb5eb447b95c
rodrigozx <rodrigozx@hotmail.com>
2017-04-30 22:27:24 -0300

    first commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,1 @@
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
-
-export default rootReducer;
+import  {combineReducers} from 'redux';
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,14 @@
+//creo una funcion que devuelve un lista de libros
+
+export default function (){
+ 
+  return[
+    {title: 'El libro de la selva'},
+    {title: 'Hamblet'},
+    {title: 'De la tierra a la luna'},   
+    {title: 'El codigo da vinci'}
+  ]
+}
+
+
+
