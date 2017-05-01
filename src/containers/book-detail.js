5c369928d8b44e54f72375db6b74b12b62914623
rodrigozx <rodrigozx@hotmail.com>
2017-05-01 12:32:53 -0300

    update

---
+++ src/containers/book-detail.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component{
+    render(){
+        if (!this.props.book){
+            return <div>Selecciona un libro para empezar.</div>
+        }
+        return (
+            <div>
+                <h3>Detalles del libro:</h3>
+                <div>{this.props.book.title}</div>
+            </div>
+      );
+    }
+}
+
+function mapStateToProps(state){
+    return{
+        book: state.activeBook
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
